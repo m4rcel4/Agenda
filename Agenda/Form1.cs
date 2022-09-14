bd5ede4dc0606ede5e093d0d38b160c75d21415c
Marcela <111135606+m4rcel4@users.noreply.github.com>
2022-09-14 08:26:55 -0300

    Adicionar arquivos de projeto.

---
+++ Agenda/Form1.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agenda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            mostrar();
+           
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+
+            using (MySqlConnection cnn = new MySqlConnection())
+            {
+                cnn.ConnectionString = "server=localhost;database=agenda;uid=root;pwd=;port=3306";
+                cnn.Open();
+                MessageBox.Show("Inserido com sucesso!");
+                string sql = "insert into contatos (nome, email) values ('" + txtNome.Text + "', '" + txtEmail.Text + "')";
+                MySqlCommand cmd = new MySqlCommand(sql, cnn);
+                cmd.ExecuteNonQuery();
+
+
+            }
+
+        }
+
+        void mostrar()
+        {
+            try
+            {
+                using (MySqlConnection cnn = new MySqlConnection())
+                {
+                    cnn.ConnectionString = "server=localhost;database=agenda;uid=root;pwd=;port=3306";
+                    cnn.Open();
+                    string sql = "Select * from contatos";
+                    DataTable table = new DataTable();
+                    MySqlDataAdapter adpter = new MySqlDataAdapter(sql, cnn);
+                    adpter.Fill(table);
+                    dgwTabela.DataSource = table;
+
+                    dgwTabela.AutoGenerateColumns = false;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            mostrar();
+
+        }
+    }
+}
